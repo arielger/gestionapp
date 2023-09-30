eb4dfdb9becc3d4f4a6ec82bc58740561f732d9c
Ariel Gerstein <ariel.gers@gmail.com>
2023-09-30 17:37:07 -0300

    fix: connect to first real state agency when sign up

--- src/auth/mutations/signup.ts
+++ src/auth/mutations/signup.ts
@@ -5,13 +5,28 @@ import { Role } from "types"
 import { Signup } from "../schemas"
 
 export default resolver.pipe(resolver.zod(Signup), async ({ email, password }, ctx) => {
+  // TODO: Review - right now we are asigning the first real state agency as the user membership
+  const firstRealStateAgency = await db.organization.findFirst()
+
+  if (!firstRealStateAgency) {
+    throw new Error("No real state agency found.")
+  }
+
   const hashedPassword = await SecurePassword.hash(password.trim())
   const user = await db.user.create({
-    // TO REVIEW: Hardcoded realStateAgencyId
-    data: { email: email.toLowerCase().trim(), hashedPassword, role: "USER", realStateAgencyId: 3 },
+    data: {
+      email: email.toLowerCase().trim(),
+      hashedPassword,
+      role: "USER",
+      memberships: { create: { organization: { connect: { id: firstRealStateAgency?.id } } } },
+    },
     select: { id: true, name: true, email: true, role: true },
   })
 
-  await ctx.session.$create({ userId: user.id, role: user.role as Role })
+  await ctx.session.$create({
+    userId: user.id,
+    role: user.role as Role,
+    orgId: firstRealStateAgency.id,
+  })
   return user
 })
