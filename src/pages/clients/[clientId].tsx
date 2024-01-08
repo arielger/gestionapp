8c8433553578317d9c6b6eeebc35937bd5079ece
Ariel Gerstein <ariel.gers@gmail.com>
2024-01-08 12:52:34 -0300

    add not found pages

--- src/pages/clients/[clientId].tsx
+++ src/pages/clients/[clientId].tsx
@@ -11,17 +11,50 @@ import { PageHeader } from "src/layout/components/PageHeader"
 import { Anchor, Button, Center, Flex, Loader, Paper } from "@mantine/core"
 import { DetailsList } from "src/core/components/DetailsList"
 import { getPersonFullName } from "src/clients/utils"
+import { NotFoundError } from "blitz"
+import { NotFound } from "src/core/components/NotFound"
 
 export const Client = () => {
   const router = useRouter()
   const clientId = useParam("clientId", "number")
 
   const [deleteClientMutation] = useMutation(deleteClient)
-  const [client, { isLoading }] = useQuery(getClient, { id: clientId })
+  const [client, { isLoading: isLoadingClient, error }] = useQuery(
+    getClient,
+    {
+      id: clientId,
+    },
+    {
+      suspense: false,
+      refetchOnWindowFocus: false,
+    }
+  )
 
-  const fullName = getPersonFullName(client)
+  const notFound = (error as NotFoundError)?.name === NotFoundError.name
 
   // TODO: add type of client (owner / tenant)
+
+  if (notFound) {
+    return (
+      <NotFound
+        title="Cliente no encontrado"
+        description={
+          "No encontramos el cliente que estás buscando.\n Por favor, verificá el ID ingresado"
+        }
+        goBackRoute={Routes.ClientsPage()}
+      />
+    )
+  }
+
+  if (isLoadingClient || !client) {
+    return (
+      <Center>
+        <Loader mt={160} />
+      </Center>
+    )
+  }
+
+  const fullName = getPersonFullName(client)
 
   return (
     <>
@@ -67,24 +100,18 @@ export const Client = () => {
           </Flex>
         </PageHeader>
         <Paper shadow="xs" p="lg">
-          {isLoading || !client ? (
-            <Center>
-              <Loader />
-            </Center>
-          ) : (
-            <DetailsList
-              details={[
-                {
-                  title: "Nombre completo",
-                  value: fullName,
-                },
-                {
-                  title: "Email",
-                  value: client.email ?? "-",
-                },
-              ]}
-            />
-          )}
+          <DetailsList
+            details={[
+              {
+                title: "Nombre completo",
+                value: fullName,
+              },
+              {
+                title: "Email",
+                value: client.email ?? "-",
+              },
+            ]}
+          />
         </Paper>
       </div>
     </>
